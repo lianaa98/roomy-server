8a0c7f49d865f44b3f13a1f089010db7c9212af8
lianaa98 <liana.kan98@gmail.com>
2023-07-19 13:35:57 -0700

    initial commit

---
+++ src/test/java/com/lianaa98/roomy/RoomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lianaa98.roomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
