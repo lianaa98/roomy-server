3b66d4bd92aee50d2697c26ea778eccc2235c12c
lianaa98 <liana.kan98@gmail.com>
2023-08-01 13:37:15 -0700

    allow cors

--- src/main/java/com/lianaa98/roomy/RoomyApplication.java
+++ src/main/java/com/lianaa98/roomy/RoomyApplication.java
@@ -1,7 +1,11 @@
 package com.lianaa98.roomy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RoomyApplication {
@@ -10,4 +14,19 @@ public class RoomyApplication {
 		SpringApplication.run(RoomyApplication.class, args);
 	}
 
+	// allow cors
+	@Value("${client.url}")
+	private String clientUrl;
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(clientUrl);
+			}
+		};
+	}
 }
+
+
